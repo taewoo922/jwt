631e45470105319a7b68ae4f861ddf652b6f31b0
taewoo922 <kimtaewoo922@gmail.com>
2024-06-17 15:41:15 +0900

    security lambda

--- src/main/java/com/example/jwt/global/security/SecurityConfig.java
+++ src/main/java/com/example/jwt/global/security/SecurityConfig.java
@@ -27,10 +27,18 @@ public class SecurityConfig {
                                 .requestMatchers("/api/*/member/login").permitAll() // 로그인은 누구나 가능
                                 .anyRequest().authenticated() // 나머지는 인증된 사용자만 가능
                 )
-                .cors().disable() // 타 도메인에서 API 호출 가능
-                .csrf().disable() // CSRF 토큰 끄기
-                .httpBasic().disable() // httpBaic 로그인 방식 끄기
-                .formLogin().disable() // 폼 로그인 방식 끄기
+                .cors(
+                        cors -> cors.disable()
+                ) // 타 도메인에서 API 호출 가능
+                .csrf(
+                        csrf -> csrf.disable()
+                ) // CSRF 토큰 끄기
+                .httpBasic(
+                        httpBasic -> httpBasic.disable()
+                ) // httpBaic 로그인 방식 끄기
+                .formLogin(
+                        formLogin -> formLogin.disable()
+                ) // 폼 로그인 방식 끄기
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(STATELESS)
                 ) // 세션끄기
